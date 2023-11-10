bd6988cdfde048013441675d216d755b6d1258f0
Peter Lord <im@ptrlrd.com>
2023-11-09 22:26:34 -0800

    Add some content to README.md

--- bot/bot.py
+++ bot/bot.py
@@ -1,29 +1,45 @@
 from nextcord.ext import commands
 import nextcord
-from shared.config import CHANNEL_ID
+from shared.config import CHANNEL_ID, DISCORD_TOKEN, GUILD_ID
 
+# Initialize the bot with the appropriate intents
 intents = nextcord.Intents.all()
 bot = commands.Bot(command_prefix="!", intents=intents)
 
 
-@bot.command()
-async def hello(ctx):
-    await ctx.send("Hello World!")
+# Define the clear_channel function
+async def clear_channel(channel):
+    await channel.purge()
+
+
+# Define the update_message function
+async def update_message(content):
+    channel = bot.get_channel(CHANNEL_ID)
+    # Clear the channel
+    await clear_channel(channel)
+
+    # Split the content and send separate messages
+    sections = content.split("## ")
+    for section in sections:
+        # Ensure section is not empty or just whitespace
+        if section and not section.isspace():
+            await channel.send(f"## {section}")
+
+
+# Define your slash commands and events below
+list_of_guilds = [GUILD_ID]  # Replace with your actual guild IDs
+bot.test_guilds = list_of_guilds  # Apply the list of test guilds to the bot instance
+
+
+@bot.slash_command(name="hello", description="Says hello")
+async def hello(interaction: nextcord.Interaction):
+    await interaction.response.send_message("Hello World!", ephemeral=True)
 
 
 @bot.event
 async def on_ready():
     print('Bot is ready.')
 
-
-async def clear_channel(channel):
-    await channel.purge()
-
-
-async def update_message(content):
-    channel = bot.get_channel(CHANNEL_ID)
-    await clear_channel(channel)
-    sections = content.split("## ")
-    for section in sections:
-        if section and not section.isspace():
-            await channel.send(f"## {section}")
+# The main execution point
+if __name__ == '__main__':
+    bot.run(DISCORD_TOKEN)
--- shared/config.py
+++ shared/config.py
@@ -1,6 +1,7 @@
 from dotenv import load_dotenv
 import os
 
+GUILD_ID = 930170875049820181
 CHANNEL_ID = 978644045519802378  # This will be imported in both bot and web modules
 DISCORD_TOKEN = os.getenv('DISCORD_TOKEN')
 PREFIX = os.getenv('PREFIX')
