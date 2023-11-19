3d48718e3bc304b1e8cb8f05ec6d1e81582560f1
Peter Lord <im@ptrlrd.com>
2023-11-18 20:45:51 -0800

    Refactor slash commands and move staff roles to config

--- bot/slash_commands.py
+++ bot/slash_commands.py
@@ -1,54 +1,107 @@
 import requests
-from nextcord import Interaction, ui, ButtonStyle, SlashOption
+from nextcord import Interaction, ui, ButtonStyle, SlashOption, Member, Embed
 from nextcord.ext import commands
 
 from bot.bot import bot, update_message
+from shared.config import STAFF_ROLES
 
-STAFF_ROLES = ["Staff", "Senior Staff", "Root"]  # Replace with actual role names or IDs
 
-
-@bot.slash_command(name="hello", description="Says hello")
-async def hello(interaction: Interaction):
+class CoreCommandCogs(commands.Cog):
     """
-    A slash command that sends a 'Hello World!' message.
+    Cog for server interaction commands.
+    """
+
+    def __init__(self, bot):
+        self.bot = bot
+        self.update_message = bot.update_message
+
+    @bot.slash_command(name="hello", description="Says hello")
+    async def hello(self, interaction: Interaction):
+        await interaction.response.send_message("Hello World!", ephemeral=True)
+
+    @bot.slash_command(name="vote", description="Vote for the server on different platforms!")
+    async def vote(self, interaction: Interaction):
+        view = ui.View()
+        # Add voting platform buttons to the view
+        view.add_item(ui.Button(style=ButtonStyle.url, label="Top.gg", url="https://top.gg/servers/930170875049820181"
+                                                                           "#reviews"))
+        view.add_item(ui.Button(style=ButtonStyle.url, label="Disboard", url="https://disboard.org/server"
+                                                                             "/930170875049820181"))
+        view.add_item(ui.Button(style=ButtonStyle.url, label="Discords", url="https://discords.com/servers/dev"))
+        await interaction.response.send_message("Please vote for us!", view=view, ephemeral=True)
+
+    @bot.slash_command(name="update_resources", description="Updates the message in #resources.")
+    @commands.has_any_role(*STAFF_ROLES)
+    async def update_command(self, interaction: Interaction):
+        await interaction.response.defer(ephemeral=True)
+        response = requests.get('https://raw.githubusercontent.com/ptrlrd/dpc-resources/main/resources.txt')
+        content = response.text
+        await self.update_message(content)
+        await interaction.followup.send("Resources updated successfully!", ephemeral=True)
+
+
+class UtilityCog(commands.Cog):
+    """
+    A utility cog for the bot, providing basic utility commands.
+
+    This cog includes commands like ping, echo, and userinfo which serve
+    as utility functions for users interacting with the bot.
+    """
+
+    def __init__(self, bot):
+        """
+        Initialize the UtilityCog with a reference to the bot.
+
+        Args:
+        bot: The instance of the bot that the cog is being added to.
+        """
+        self.bot = bot
+
+    @bot.slash_command(name="ping", description="Check the bot's latency")
+    async def ping(self, interaction: Interaction):
+        """
+        A slash command to check the bot's current latency.
+
+        Args:
+        interaction (Interaction): The interaction that triggered the command.
+        """
+        await interaction.response.send_message(f"Pong! Latency: {round(self.bot.latency * 1000)}ms")
+
+    @bot.slash_command(name="echo", description="Repeat a message")
+    async def echo(self, interaction: Interaction, message: str = SlashOption(description="The message to repeat")):
+        """
+        A slash command to repeat a message sent by the user.
+
+        Args:
+        interaction (Interaction): The interaction that triggered the command.
+        message (str): The message to be repeated.
+        """
+        await interaction.response.send_message(message)
+
+    @bot.slash_command(name="userinfo", description="Get information about a user")
+    async def userinfo(self, interaction: Interaction,
+                       user: Member = SlashOption(description="The user to get info about")):
+        """
+        A slash command to display information about a Discord user.
+
+        Args:
+        interaction (Interaction): The interaction that triggered the command.
+        user (Member): The Discord member whose information is to be displayed.
+        """
+        embed = Embed(title="User Information", color=0x00ff00)
+        embed.add_field(name="Username", value=user.name, inline=False)
+        embed.add_field(name="ID", value=user.id, inline=False)
+        embed.add_field(name="Joined at", value=user.joined_at, inline=False)
+        embed.set_thumbnail(url=user.avatar.url)
+        await interaction.response.send_message(embed=embed)
+
+
+def setup(bot):
+    """
+    Setup function to add this cog to a bot.
 
     Args:
-    interaction (Interaction): The interaction that triggered the command.
+    bot: The bot instance to which the cog is being added.
     """
-    await interaction.response.send_message("Hello World!", ephemeral=True)
-
-
-@bot.slash_command(name="vote", description="Vote for the server on different platforms!")
-async def vote(interaction: Interaction):
-    """
-    A slash command that provides buttons to vote for the server on different platforms.
-
-    Args:
-    interaction (Interaction): The interaction that triggered the command.
-    """
-    view = ui.View()
-
-    # Add voting platform buttons to the view
-    view.add_item(ui.Button(style=ButtonStyle.url, label="Top.gg", url="https://top.gg/servers/930170875049820181#reviews"))
-    view.add_item(ui.Button(style=ButtonStyle.url, label="Disboard", url="https://disboard.org/server/930170875049820181"))
-    view.add_item(ui.Button(style=ButtonStyle.url, label="Discords", url="https://discords.com/servers/dev"))
-
-    await interaction.response.send_message("Please vote for us!", view=view, ephemeral=True)
-
-
-@bot.slash_command(name="update_resources", description="Updates the message in #resources.")
-@commands.has_any_role(*STAFF_ROLES)
-async def update_command(interaction: Interaction):
-    """
-    A slash command restricted to specific roles that updates a message in the #resources channel.
-
-    Args:
-    interaction (Interaction): The interaction that triggered the command.
-    """
-    await interaction.response.defer(ephemeral=True)
-
-    response = requests.get('https://raw.githubusercontent.com/ptrlrd/dpc-resources/main/resources.txt')
-    content = response.text
-    await update_message(content)
-
-    await interaction.followup.send("Resources updated successfully!", ephemeral=True)
+    bot.add_cog(CoreCommandCogs(bot))
+    bot.add_cog(UtilityCog(bot))
