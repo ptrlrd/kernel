600319778a9d128b3080ed218c85bf508ae5fe84
Peter Lord <im@ptrlrd.com>
2024-08-31 20:47:40 -0700

    Fixing the embed

--- bot/context_menus.py
+++ bot/context_menus.py
@@ -115,7 +115,9 @@ class MessageManagementCog(commands.Cog):
             interaction (Interaction): The interaction that triggered the command.
             message (Message): The message where the command was invoked.
         """
-        await interaction.response.send_message("To use a slash command, type a forward-slash \"/\" in the chat bar and a list of commands will appear. Click on the one you want to use. For more information visit https://support.discord.com/hc/en-us/articles/1500000368501-Slash-Commands-FAQ.  https://support.discord.com/hc/article_attachments/1500003352961")
+        message_content = "To use a slash command, type a forward-slash \"/\" in the chat bar and a list of commands will appear. Click on the one you want to use. For more information visit https://support.discord.com/hc/en-us/articles/1500000368501-Slash-Commands-FAQ."
+        file = nextcord.File("../images/slashcommand.png", filename="slashcommand.png")
+        await interaction.response.send_message(content=message_content, file=file)
 
 
 def setup(bot):
