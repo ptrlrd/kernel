b5103fc6be689a82e32240cde8963484a97d7bf0
Peter Lord <im@ptrlrd.com>
2024-11-08 15:24:43 -0800

    Add prometheus server func

--- main.py
+++ main.py
@@ -45,5 +45,5 @@ if __name__ == '__main__':
     bot.load_extension('bot.context_menus')
     bot.load_extension('bot.news_feed')
     # bot.load_extension('bot.moderation') Turning off because it's not working
-    # Start the bot
+    start_prometheus_server()
     bot.run(DISCORD_TOKEN)
